354589d7cbcd903e005bf1f33a8a83494e9f8d43
alexus21 <US21003@ues.edu.sv>
2023-09-04 16:17:21 -0600

    Mostrando resultados en CheckSumForm

--- BinaryDataManagement/Forms/CheckSumForm.Designer.cs
+++ BinaryDataManagement/Forms/CheckSumForm.Designer.cs
@@ -26,17 +26,24 @@
             this.lblMessage = new System.Windows.Forms.Label();
             this.btnSendData = new System.Windows.Forms.Button();
             this.txtEnterData = new System.Windows.Forms.TextBox();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.lblSentData = new System.Windows.Forms.Label();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.lblDataSentResult = new System.Windows.Forms.Label();
+            this.lblBinarySum = new System.Windows.Forms.Label();
+            this.lblCheckSum = new System.Windows.Forms.Label();
+            this.lblFinalResult = new System.Windows.Forms.Label();
+            this.lblBinarySumResult = new System.Windows.Forms.Label();
+            this.lblCheckSumResult = new System.Windows.Forms.Label();
+            this.lblShowFinalResult = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lblMessage
             // 
             this.lblMessage.AutoSize = true;
-            this.lblMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblMessage.Location = new System.Drawing.Point(41, 36);
             this.lblMessage.Name = "lblMessage";
-            this.lblMessage.Size = new System.Drawing.Size(255, 16);
+            this.lblMessage.Size = new System.Drawing.Size(304, 20);
             this.lblMessage.TabIndex = 0;
             this.lblMessage.Text = "Por favor, ingrese aquí el dato a transmitir:";
             // 
@@ -61,29 +68,99 @@
             this.txtEnterData.TabIndex = 2;
             this.txtEnterData.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtEnterData_KeyPress);
             // 
-            // dataGridView1
+            // lblSentData
             // 
-            this.dataGridView1.AllowUserToAddRows = false;
-            this.dataGridView1.AllowUserToDeleteRows = false;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(44, 125);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.ReadOnly = true;
-            this.dataGridView1.Size = new System.Drawing.Size(477, 144);
-            this.dataGridView1.TabIndex = 3;
+            this.lblSentData.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSentData.Location = new System.Drawing.Point(44, 135);
+            this.lblSentData.Name = "lblSentData";
+            this.lblSentData.Size = new System.Drawing.Size(138, 27);
+            this.lblSentData.TabIndex = 0;
+            this.lblSentData.Text = "Dato transmitido:";
+            // 
+            // lblDataSentResult
+            // 
+            this.lblDataSentResult.AutoSize = true;
+            this.lblDataSentResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDataSentResult.Location = new System.Drawing.Point(260, 135);
+            this.lblDataSentResult.Name = "lblDataSentResult";
+            this.lblDataSentResult.Size = new System.Drawing.Size(0, 20);
+            this.lblDataSentResult.TabIndex = 3;
+            // 
+            // lblBinarySum
+            // 
+            this.lblBinarySum.AutoSize = true;
+            this.lblBinarySum.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBinarySum.Location = new System.Drawing.Point(44, 183);
+            this.lblBinarySum.Name = "lblBinarySum";
+            this.lblBinarySum.Size = new System.Drawing.Size(145, 20);
+            this.lblBinarySum.TabIndex = 4;
+            this.lblBinarySum.Text = "Suma de los datos:";
+            // 
+            // lblCheckSum
+            // 
+            this.lblCheckSum.AutoSize = true;
+            this.lblCheckSum.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCheckSum.Location = new System.Drawing.Point(44, 231);
+            this.lblCheckSum.Name = "lblCheckSum";
+            this.lblCheckSum.Size = new System.Drawing.Size(172, 20);
+            this.lblCheckSum.TabIndex = 5;
+            this.lblCheckSum.Text = "CheckSum de la suma:";
+            // 
+            // lblFinalResult
+            // 
+            this.lblFinalResult.AutoSize = true;
+            this.lblFinalResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFinalResult.Location = new System.Drawing.Point(44, 275);
+            this.lblFinalResult.Name = "lblFinalResult";
+            this.lblFinalResult.Size = new System.Drawing.Size(86, 20);
+            this.lblFinalResult.TabIndex = 6;
+            this.lblFinalResult.Text = "Resultado:";
+            // 
+            // lblBinarySumResult
+            // 
+            this.lblBinarySumResult.AutoSize = true;
+            this.lblBinarySumResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBinarySumResult.Location = new System.Drawing.Point(260, 183);
+            this.lblBinarySumResult.Name = "lblBinarySumResult";
+            this.lblBinarySumResult.Size = new System.Drawing.Size(0, 20);
+            this.lblBinarySumResult.TabIndex = 7;
+            // 
+            // lblCheckSumResult
+            // 
+            this.lblCheckSumResult.AutoSize = true;
+            this.lblCheckSumResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCheckSumResult.Location = new System.Drawing.Point(260, 231);
+            this.lblCheckSumResult.Name = "lblCheckSumResult";
+            this.lblCheckSumResult.Size = new System.Drawing.Size(0, 20);
+            this.lblCheckSumResult.TabIndex = 7;
+            // 
+            // lblShowFinalResult
+            // 
+            this.lblShowFinalResult.AutoSize = true;
+            this.lblShowFinalResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblShowFinalResult.Location = new System.Drawing.Point(260, 275);
+            this.lblShowFinalResult.Name = "lblShowFinalResult";
+            this.lblShowFinalResult.Size = new System.Drawing.Size(0, 20);
+            this.lblShowFinalResult.TabIndex = 7;
             // 
             // CheckSumForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(562, 350);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.lblShowFinalResult);
+            this.Controls.Add(this.lblCheckSumResult);
+            this.Controls.Add(this.lblBinarySumResult);
+            this.Controls.Add(this.lblFinalResult);
+            this.Controls.Add(this.lblCheckSum);
+            this.Controls.Add(this.lblBinarySum);
+            this.Controls.Add(this.lblDataSentResult);
+            this.Controls.Add(this.lblSentData);
             this.Controls.Add(this.txtEnterData);
             this.Controls.Add(this.btnSendData);
             this.Controls.Add(this.lblMessage);
             this.Name = "CheckSumForm";
             this.Text = "CheckSumForm";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -94,6 +171,14 @@
         private System.Windows.Forms.Label lblMessage;
         private System.Windows.Forms.Button btnSendData;
         private System.Windows.Forms.TextBox txtEnterData;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label lblSentData;
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.Label lblDataSentResult;
+        private System.Windows.Forms.Label lblBinarySum;
+        private System.Windows.Forms.Label lblCheckSum;
+        private System.Windows.Forms.Label lblFinalResult;
+        private System.Windows.Forms.Label lblBinarySumResult;
+        private System.Windows.Forms.Label lblCheckSumResult;
+        private System.Windows.Forms.Label lblShowFinalResult;
     }
 }
--- BinaryDataManagement/Forms/CheckSumForm.cs
+++ BinaryDataManagement/Forms/CheckSumForm.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Windows.Forms;
-using BinaryDataManagement.DataCorrection.CheckSum;
 
 namespace BinaryDataManagement.Forms {
     public partial class CheckSumForm :Form {
@@ -36,8 +35,125 @@ namespace BinaryDataManagement.Forms {
             if (txtEnterData.Text == "") {
                 MessageBox.Show("Error: debes proveer un dato a transmitir.");
             }
-
-            CheckSum.FindCheckSum(_binaryData);
+            else {
+                CheckSum(_binaryData);
+            }
+            
+            txtEnterData.Clear();
         }
+
+        void ShowResults(string binarySum, string checksum, string completeData, string result) {
+            Console.WriteLine(binarySum);
+            Console.WriteLine(checksum);
+            Console.WriteLine(completeData);
+            Console.WriteLine(result);
+
+            lblBinarySumResult.Text = binarySum;
+            lblCheckSumResult.Text = checksum;
+            lblDataSentResult.Text = completeData;
+            lblShowFinalResult.Text = result;
+        }
+
+        #region CheckSumAlgorithm
+        #region MainPart
+        void CheckSum(string binaryDataString) {
+            const int maxBits = 6;
+            
+            Console.WriteLine("\n");
+
+            // Primero, comprobamos que sea de 6 bits; caso contrario, rellenamos con 0s:
+            if (binaryDataString.Length % maxBits != 0) {
+                binaryDataString = FillWithZeros(binaryDataString);
+            }
+            
+            // Ahora, procedemos a hacer la suma binaria:
+            string myBinaryDataSender = Sum(binaryDataString);
+            string myCheckSum = ReturnCheckSum(myBinaryDataSender);
+            string sentValue = myCheckSum + myBinaryDataSender;
+            string result = FindMistakes(sentValue);
+
+            bool isAllOk = true;
+            
+            for (int i = 0; i < result.Length; i++) {
+                if (result[i] != '0') {
+                    isAllOk = false;
+                }
+            }
+
+            if (isAllOk) result += " (todo correcto)";
+            else result += " (error de transmisión)";
+
+            ShowResults(myBinaryDataSender, myCheckSum, sentValue, result);
+        }
+        #endregion
+        
+        #region FillString
+        string FillWithZeros(string bdq) {
+            while (bdq.Length % 6 != 0) {
+                bdq = "0" + bdq;
+            }
+
+            return bdq;
+        }
+        #endregion
+        
+        #region BinarySum
+        string Sum(string binaryData) {
+
+            // Dividir la cadena en subcadenas de 6 bits c/u
+            List<string> subStrings = new List<string>();
+            for (int i = 0; i < binaryData.Length; i += 6) {
+                subStrings.Add(binaryData.Substring(i, 6));
+            }
+
+            int[] result = new int[6];
+            int carry = 0;
+
+            // Sumar los elementos de cada posición aplicando la lógica de suma binaria
+            foreach (string s in subStrings) {
+                for (int i = 5, j = s.Length - 1; i >= 0; i--, j--) {
+                    int valor = int.Parse(s[j].ToString()) + result[i] + carry;
+                    result[i] = valor % 2;
+                    carry = valor / 2;
+                }
+            }
+
+            // Convertir el resultado en una cadena de 0s y 1s
+            string resultStr = string.Join("", result);
+            return resultStr;
+        }
+        #endregion
+        
+        #region FindCheckSum
+        string ReturnCheckSum(string binaryData) {
+            
+            // Crear una cadena mutable para realizar los reemplazos
+            char[] modifiedData = binaryData.ToCharArray();
+
+            for (int i = 0; i < modifiedData.Length; i++) {
+                if (modifiedData[i] == '0') {
+                    modifiedData[i] = '1';
+                }
+                else if (modifiedData[i] == '1') {
+                    modifiedData[i] = '0';
+                }
+            }
+            
+            // Convertir la cadena mutable de nuevo a una cadena
+            string result = new string(modifiedData);
+            
+            return result;
+        }
+        #endregion
+        
+        #region VerifyData
+        string FindMistakes(string data) {
+            string result = Sum(data);
+            string hasMistakes = ReturnCheckSum(result);
+            return hasMistakes;
+        }
+        
+        #endregion
+        #endregion
     }
 }
