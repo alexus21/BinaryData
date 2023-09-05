fdd019e61e33e8de737ce0e851c21400c5414167
alexus21 <US21003@ues.edu.sv>
2023-09-05 16:07:43 -0600

    Agregando textfield de dato reibido para comprobación

--- BinaryDataManagement/Forms/CheckSumForm.Designer.cs
+++ BinaryDataManagement/Forms/CheckSumForm.Designer.cs
@@ -23,87 +23,120 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.lblMessage = new System.Windows.Forms.Label();
-            this.btnSendData = new System.Windows.Forms.Button();
-            this.txtEnterData = new System.Windows.Forms.TextBox();
+            this.lblSentData = new System.Windows.Forms.Label();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.txtSent = new System.Windows.Forms.TextBox();
             this.fontDialog1 = new System.Windows.Forms.FontDialog();
             this.dataGridView = new System.Windows.Forms.DataGridView();
-            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            this.lblReceivedData = new System.Windows.Forms.Label();
+            this.txtReceived = new System.Windows.Forms.TextBox();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
             this.SuspendLayout();
             // 
-            // lblMessage
+            // lblSentData
             // 
-            this.lblMessage.AutoSize = true;
-            this.lblMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblMessage.Location = new System.Drawing.Point(153, 44);
-            this.lblMessage.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.lblMessage.Name = "lblMessage";
-            this.lblMessage.Size = new System.Drawing.Size(358, 24);
-            this.lblMessage.TabIndex = 0;
-            this.lblMessage.Text = "Por favor, ingrese aquí el dato a transmitir:";
+            this.lblSentData.AutoSize = true;
+            this.lblSentData.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSentData.Location = new System.Drawing.Point(9, 21);
+            this.lblSentData.Name = "lblSentData";
+            this.lblSentData.Size = new System.Drawing.Size(369, 18);
+            this.lblSentData.TabIndex = 0;
+            this.lblSentData.Text = "Por favor, ingrese aquí el dato enviado (máximo 6 bits):";
             // 
-            // btnSendData
+            // btnCheck
             // 
-            this.btnSendData.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSendData.Location = new System.Drawing.Point(658, 90);
-            this.btnSendData.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.btnSendData.Name = "btnSendData";
-            this.btnSendData.Size = new System.Drawing.Size(135, 41);
-            this.btnSendData.TabIndex = 1;
-            this.btnSendData.Text = "Transmitir";
-            this.btnSendData.UseVisualStyleBackColor = true;
-            this.btnSendData.Click += new System.EventHandler(this.btnSendData_Click);
+            this.btnCheck.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCheck.Location = new System.Drawing.Point(659, 12);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(101, 33);
+            this.btnCheck.TabIndex = 1;
+            this.btnCheck.Text = "Verificar";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
             // 
-            // txtEnterData
+            // txtSent
             // 
-            this.txtEnterData.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtEnterData.Location = new System.Drawing.Point(157, 90);
-            this.txtEnterData.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.txtEnterData.Multiline = true;
-            this.txtEnterData.Name = "txtEnterData";
-            this.txtEnterData.Size = new System.Drawing.Size(492, 37);
-            this.txtEnterData.TabIndex = 2;
-            this.txtEnterData.Enter += new System.EventHandler(this.txtEnterData_Enter);
-            this.txtEnterData.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtEnterData_KeyPress);
+            this.txtSent.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSent.Location = new System.Drawing.Point(384, 12);
+            this.txtSent.Multiline = true;
+            this.txtSent.Name = "txtSent";
+            this.txtSent.Size = new System.Drawing.Size(219, 31);
+            this.txtSent.TabIndex = 2;
+            this.txtSent.TextChanged += new System.EventHandler(this.txtSent_TextChanged);
+            this.txtSent.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtSent_KeyPress);
             // 
             // dataGridView
             // 
             this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Column1});
-            this.dataGridView.Location = new System.Drawing.Point(114, 177);
-            this.dataGridView.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dataGridView.Location = new System.Drawing.Point(12, 170);
             this.dataGridView.Name = "dataGridView";
             this.dataGridView.RowHeadersWidth = 51;
-            this.dataGridView.Size = new System.Drawing.Size(717, 367);
+            this.dataGridView.Size = new System.Drawing.Size(748, 284);
             this.dataGridView.TabIndex = 8;
-            // 
-            // Column1
-            // 
-            this.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.Column1.HeaderText = "Suma de datos";
-            this.Column1.MinimumWidth = 6;
-            this.Column1.Name = "Column1";
             // 
             // fileSystemWatcher1
             // 
             this.fileSystemWatcher1.EnableRaisingEvents = true;
             this.fileSystemWatcher1.SynchronizingObject = this;
             // 
+            // lblReceivedData
+            // 
+            this.lblReceivedData.AutoSize = true;
+            this.lblReceivedData.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblReceivedData.Location = new System.Drawing.Point(9, 96);
+            this.lblReceivedData.Name = "lblReceivedData";
+            this.lblReceivedData.Size = new System.Drawing.Size(370, 18);
+            this.lblReceivedData.TabIndex = 0;
+            this.lblReceivedData.Text = "Por favor, ingrese aquí el dato recibido (máximo 6 bits):";
+            // 
+            // txtReceived
+            // 
+            this.txtReceived.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtReceived.Location = new System.Drawing.Point(384, 92);
+            this.txtReceived.Multiline = true;
+            this.txtReceived.Name = "txtReceived";
+            this.txtReceived.Size = new System.Drawing.Size(219, 31);
+            this.txtReceived.TabIndex = 2;
+            this.txtReceived.TextChanged += new System.EventHandler(this.txtReceived_TextChanged);
+            this.txtReceived.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtReceived_KeyPress);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.Location = new System.Drawing.Point(659, 96);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(101, 33);
+            this.btnReset.TabIndex = 1;
+            this.btnReset.Text = "Reiniciar";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // Column1
+            // 
+            this.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column1.HeaderText = "Resultados";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            // 
             // CheckSumForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1113, 574);
+            this.ClientSize = new System.Drawing.Size(771, 466);
             this.Controls.Add(this.dataGridView);
-            this.Controls.Add(this.txtEnterData);
-            this.Controls.Add(this.btnSendData);
-            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.txtReceived);
+            this.Controls.Add(this.txtSent);
+            this.Controls.Add(this.lblReceivedData);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.lblSentData);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.Name = "CheckSumForm";
             this.Text = "CheckSum";
@@ -116,12 +149,15 @@
 
         #endregion
 
-        private System.Windows.Forms.Label lblMessage;
-        private System.Windows.Forms.Button btnSendData;
-        private System.Windows.Forms.TextBox txtEnterData;
+        private System.Windows.Forms.Label lblSentData;
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.TextBox txtSent;
         private System.Windows.Forms.FontDialog fontDialog1;
         private System.Windows.Forms.DataGridView dataGridView;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
         private System.IO.FileSystemWatcher fileSystemWatcher1;
+        private System.Windows.Forms.TextBox txtReceived;
+        private System.Windows.Forms.Label lblReceivedData;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
     }
 }
--- BinaryDataManagement/Forms/CheckSumForm.cs
+++ BinaryDataManagement/Forms/CheckSumForm.cs
@@ -2,66 +2,110 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Drawing;
+using System.Linq;
 using System.Windows.Forms;
 
 namespace BinaryDataManagement.Forms {
     public partial class CheckSumForm :Form {
 
         // Acá se almacenarán los datos a transmitir:
-        string _binaryData;
+        string _binaryDataSent, _binaryDataReceived;
 
         public CheckSumForm() {
             InitializeComponent();
+            btnReset.Enabled = false;
         }
 
-        void txtEnterData_KeyPress(object sender, KeyPressEventArgs e) {
-
-            if (!char.IsDigit(e.KeyChar)) {
-                e.Handled = true;
-                MessageBox.Show("Solo números están permitidos");
-                return;
-            }
-
+        void txtSent_KeyPress(object sender, KeyPressEventArgs e) {
             // Si se ingresan valores diferentes a 0 y 1:
-            if (e.KeyChar != '0' && e.KeyChar != '1') {
+            if (e.KeyChar != '0' && e.KeyChar != '1' && e.KeyChar != (char)Keys.Back) {
                 e.Handled = true;
                 MessageBox.Show("Solo se permiten datos binarios (0s y 1s)");
                 return;
             }
 
-            // Todo OK, guardar a la cadena:
-            _binaryData += e.KeyChar.ToString();
+            _binaryDataSent += e.KeyChar.ToString();
         }
 
-        void btnSendData_Click(object sender, EventArgs e) {
-            
-            if (txtEnterData.Text == "") {
-                MessageBox.Show("Error: debes proveer un dato a transmitir.");
-            } else {
-                dataGridView.Rows.Clear();
-                dataGridView.Rows.Add("Dato transmitido: " + _binaryData);
-                CheckSum(_binaryData);
+        void txtReceived_KeyPress(object sender, KeyPressEventArgs e) {
+            // Si se ingresan valores diferentes a 0 y 1:
+            if (e.KeyChar != '0' && e.KeyChar != '1' && e.KeyChar != (char)Keys.Back) {
+                e.Handled = true;
+                MessageBox.Show("Solo se permiten datos binarios (0s y 1s)");
+                return;
             }
 
-            _binaryData = "";
-            txtEnterData.Clear();
+            _binaryDataReceived += e.KeyChar.ToString();
         }
 
-        void txtEnterData_Enter(object sender, EventArgs e) {
-            // SendItems();
+        void txtSent_TextChanged(object sender, EventArgs e) {
+            // Elimina cualquier carácter no válido
+            _binaryDataSent = new string(txtSent.Text.Where(c => c == '0' || c == '1').ToArray());
+
+            // Limita la longitud a 6 dígitos
+            if (_binaryDataSent.Length > 6) {
+                MessageBox.Show("Límite de bits alcanzado");
+                _binaryDataSent = _binaryDataSent.Substring(0, 6);
+            }
+            
+            // Actualiza el valor del cuadro de texto
+            txtSent.Text = _binaryDataSent; 
+            // Coloca el cursor al final del texto
+            txtSent.SelectionStart = _binaryDataSent.Length; 
         }
 
-        void ShowResults(string binarySum, string checksum, string completeData, string result) {
+        void txtReceived_TextChanged(object sender, EventArgs e) {
+            // Elimina cualquier carácter no válido
+            _binaryDataReceived = new string(txtReceived.Text.Where(c => c == '0' || c == '1').ToArray());
+
+            // Limita la longitud a 6 dígitos
+            if (_binaryDataReceived.Length > 6) {
+                MessageBox.Show("Límite de bits alcanzado");
+                _binaryDataReceived = _binaryDataReceived.Substring(0, 6);
+            }
+            
+            // Actualiza el valor del cuadro de texto
+            txtReceived.Text = _binaryDataReceived; 
+            // Coloca el cursor al final del texto
+            txtReceived.SelectionStart = _binaryDataReceived.Length; 
+        }
+
+        void btnCheck_Click(object sender, EventArgs e) {
+            
+            if (txtSent.Text == "" || txtReceived.Text == "") {
+                MessageBox.Show("Error: debes proveer un dato a transmitir.");
+            } else {
+                btnCheck.Enabled = false;
+                dataGridView.Rows.Add("Dato transmitido: " + FillWithZeros(_binaryDataSent));
+                dataGridView.Rows.Add("Dato recibido: " + FillWithZeros(_binaryDataReceived));
+                CheckSum(_binaryDataSent, _binaryDataReceived);
+                txtSent.Enabled = false;
+                txtReceived.Enabled = false;
+                btnReset.Enabled = true;
+            }
+        }
+
+        void btnReset_Click(object sender, EventArgs e) {
+            _binaryDataSent = "";
+            txtSent.Clear();
+            txtReceived.Clear();
+            txtSent.Enabled = true;
+            txtReceived.Enabled = true;
+            dataGridView.Rows.Clear();
+            btnCheck.Enabled = true;
+            btnReset.Enabled = false;
+        }
+
+        void ShowResults(string binarySum, string checksum, string result) {
             UpdateDataGridFont();
     
             // Agrega las filas al DataGridView
             dataGridView.Rows.Add("Resultado de la suma: " + binarySum);
             dataGridView.Rows.Add("Checksum: " + checksum);
-            dataGridView.Rows.Add("Dato transmitido: " + completeData);
             dataGridView.Rows.Add("Resultado: " + result);
 
             // Marca las filas como no editables
-            for (int i = 0; i < 4; i++) {
+            for (int i = 0; i < dataGridView.Rows.Count; i++) {
                 dataGridView.Rows[i].ReadOnly = true;
             }
         }
@@ -72,28 +116,29 @@ namespace BinaryDataManagement.Forms {
                 c.DefaultCellStyle.Font = new Font("Microsoft Sans Serif", 14F, GraphicsUnit.Pixel);
             }
         }
-        
-        void CheckSumForm_Load(object sender, EventArgs e) {
-            txtEnterData.Focus();
-        }
 
         #region CheckSumAlgorithm
         #region MainPart
-        void CheckSum(string binaryDataString) {
+        void CheckSum(string binaryDataSent, string binaryDataReceived) {
             const int maxBits = 6;
             
             Console.WriteLine("\n");
+            
+            
 
             // Primero, comprobamos que sea de 6 bits; caso contrario, rellenamos con 0s:
-            if (binaryDataString.Length % maxBits != 0) {
-                binaryDataString = FillWithZeros(binaryDataString);
+            if (binaryDataSent.Length % maxBits != 0) {
+                binaryDataSent = FillWithZeros(binaryDataSent);
+            }
+            
+            if (binaryDataReceived.Length % maxBits != 0) {
+                binaryDataReceived = FillWithZeros(binaryDataReceived);
             }
             
             // Ahora, procedemos a hacer la suma binaria:
-            string myBinaryDataSender = Sum(binaryDataString);
-            string myCheckSum = ReturnCheckSum(myBinaryDataSender);
-            string sentValue = myCheckSum + myBinaryDataSender;
-            string result = FindMistakes(sentValue);
+            string myBinaryDataSum = Sum(binaryDataSent, binaryDataReceived);
+            string myCheckSum = ReturnCheckSum(myBinaryDataSum);
+            string result = FindMistakes(myBinaryDataSum, myCheckSum);
 
             bool isAllOk = true;
             
@@ -103,10 +148,10 @@ namespace BinaryDataManagement.Forms {
                 }
             }
             
-            if (isAllOk) result += " (todo correcto)";
-            else result += " (error de transmisión)";
+            if (isAllOk) result += " (envío correcto)";
+            else result += " (error de transmisión de datos)";
 
-            ShowResults(myBinaryDataSender, myCheckSum, sentValue, result);
+            ShowResults(myBinaryDataSum,  myCheckSum, result);
         }
         #endregion
         
@@ -115,19 +160,16 @@ namespace BinaryDataManagement.Forms {
             while (bdq.Length % 6 != 0) {
                 bdq = "0" + bdq;
             }
-
             return bdq;
         }
         #endregion
         
         #region BinarySum
-        string Sum(string binaryData) {
+        string Sum(string binaryDataSent, string binaryDataReceived) {
             List<string> subStrings = new List<string>();
-
-            // Dividir la cadena en subcadenas de 6 bits c/u
-            for (int i = 0; i < binaryData.Length; i += 6) {
-                subStrings.Add(binaryData.Substring(i, 6));
-            }
+            
+            subStrings.Add(binaryDataSent);
+            subStrings.Add(binaryDataReceived);
 
             int[] result = new int[6];
             int carry = 0;
@@ -139,14 +181,13 @@ namespace BinaryDataManagement.Forms {
                     result[i] = valor % 2;
                     carry = valor / 2;
                 }
-                // Asigna la cadena combinada al DataGridView
-                dataGridView.Rows.Add("Dato: " + s);
             }
 
             // Convertir el resultado (quitar los espacios):
             string resultStr = string.Join("", result);
             return resultStr;
         }
+        
         #endregion
         
         #region FindCheckSum
@@ -168,16 +209,27 @@ namespace BinaryDataManagement.Forms {
             string result = new string(modifiedData);
             return result;
         }
+
+
         #endregion
-        
+
         #region VerifyData
-        string FindMistakes(string data) {
-            string result = Sum(data);
+        string FindMistakes(string data, string checkSum) {
+            string result = Sum(data, checkSum);
             string hasMistakes = ReturnCheckSum(result);
             return hasMistakes;
         }
+        #endregion
+        
+        #region SolveMistake
 
-
+        string SolveErrors(string sentData, string receivedData) {
+            if (sentData != receivedData) {
+                return "Error de transmisión";
+            }
+            
+            return "XD";
+        }
         #endregion
 
         #endregion
