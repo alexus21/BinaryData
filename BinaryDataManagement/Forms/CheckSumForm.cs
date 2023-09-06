68e4cb6b597655070a1b51887a35561b6c811b9e
alexus21 <US21003@ues.edu.sv>
2023-09-06 15:56:37 -0600

    Agregada vista de posiciones donde las cadenas varían

--- BinaryDataManagement/Forms/CheckSumForm.cs
+++ BinaryDataManagement/Forms/CheckSumForm.cs
@@ -4,6 +4,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Windows.Forms;
+using BinaryDataManagement.DataCorrection.CheckSumAlgorithm;
 
 namespace BinaryDataManagement.Forms {
     public partial class CheckSumForm :Form {
@@ -75,11 +76,12 @@ namespace BinaryDataManagement.Forms {
             if (txtSent.Text == "" || txtReceived.Text == "") {
                 MessageBox.Show("Error: debes proveer un dato a transmitir.");
             } else {
+                CheckSumAlgorithm checkSumAlgorithm = new CheckSumAlgorithm();
                 dataGridView.Rows.Clear();
                 btnCheck.Enabled = false;
-                dataGridView.Rows.Add("Dato transmitido: " + FillWithZeros(_binaryDataSent));
-                dataGridView.Rows.Add("Dato recibido: " + FillWithZeros(_binaryDataReceived));
-                CheckSum(_binaryDataSent, _binaryDataReceived);
+                dataGridView.Rows.Add("Dato transmitido: " + checkSumAlgorithm.FillWithZeros(_binaryDataSent));
+                dataGridView.Rows.Add("Dato recibido: " + checkSumAlgorithm.FillWithZeros(_binaryDataReceived));
+                ShowResults(checkSumAlgorithm.CheckSum(_binaryDataSent, _binaryDataReceived));
                 txtSent.Enabled = false;
                 txtReceived.Enabled = false;
                 btnReset.Enabled = true;
@@ -97,13 +99,14 @@ namespace BinaryDataManagement.Forms {
             btnReset.Enabled = false;
         }
 
-        void ShowResults(string binarySum, string checksum, string result) {
+        void ShowResults(CheckSumAlgorithm.Results r) {
             UpdateDataGridFont();
     
             // Agrega las filas al DataGridView
-            dataGridView.Rows.Add("Resultado de la suma: " + binarySum);
-            dataGridView.Rows.Add("Checksum: " + checksum);
-            dataGridView.Rows.Add("Resultado: " + result);
+            dataGridView.Rows.Add("Resultado de la suma (recibido + checksum): " + r.MyBinaryDataSum);
+            dataGridView.Rows.Add("Checksum (transmitido): " + r.MyCheckSum);
+            dataGridView.Rows.Add("Resultado (complemento a 1): " + r.Result);
+            dataGridView.Rows.Add("Posiciones donde varía: " + r.Differences);
 
             // Marca las filas como no editables
             for (int i = 0; i < dataGridView.Rows.Count; i++) {
@@ -117,112 +120,5 @@ namespace BinaryDataManagement.Forms {
                 c.DefaultCellStyle.Font = new Font("Microsoft Sans Serif", 14F, GraphicsUnit.Pixel);
             }
         }
-
-        #region CheckSumAlgorithm
-        #region MainPart
-        void CheckSum(string binaryDataSent, string binaryDataReceived) {
-            const int maxBits = 6;
-            
-            // Primero, comprobamos que sea de 6 bits; caso contrario, rellenamos con 0s:
-            if (binaryDataSent.Length % maxBits != 0) {
-                binaryDataSent = FillWithZeros(binaryDataSent);
-            }
-            
-            if (binaryDataReceived.Length % maxBits != 0) {
-                binaryDataReceived = FillWithZeros(binaryDataReceived);
-            }
-            
-            // Ahora, procedemos a hacer la suma binaria:
-            string myCheckSum = ReturnCheckSum(binaryDataSent);
-            string myBinaryDataSum = Sum(binaryDataReceived, myCheckSum);
-            string result = ReturnCheckSum(myBinaryDataSum);
-
-            bool isAllOk = true;
-            
-            for (int i = 0; i < result.Length; i++) {
-                if (result[i] != '0') {
-                    isAllOk = false;
-                }
-            }
-            
-            if (isAllOk) result += " (envío correcto)";
-            else result += " (error de transmisión de datos)";
-
-            ShowResults(myBinaryDataSum,  myCheckSum, result);
-        }
-        #endregion
-        
-        #region FillString
-        string FillWithZeros(string bdq) {
-            while (bdq.Length % 6 != 0) {
-                bdq = "0" + bdq;
-            }
-            return bdq;
-        }
-        #endregion
-        
-        #region BinarySum
-        string Sum(string binaryDataSent, string checksum) {
-            List<string> subStrings = new List<string>();
-            
-            subStrings.Add(binaryDataSent);
-            subStrings.Add(checksum);
-
-            int[] result = new int[6];
-            int carry = 0;
-
-            // Sumar los elementos de cada posición aplicando la lógica de suma binaria
-            foreach (string s in subStrings) {
-                for (int i = 5, j = s.Length - 1; i >= 0; i--, j--) {
-                    int valor = int.Parse(s[j].ToString()) + result[i] + carry;
-                    result[i] = valor % 2;
-                    carry = valor / 2;
-                }
-            }
-
-            // Convertir el resultado (quitar los espacios):
-            string resultStr = string.Join("", result);
-            return resultStr;
-        }
-        
-        #endregion
-        
-        #region FindCheckSum
-        string ReturnCheckSum(string binaryData) {
-            
-            // Crear una cadena mutable para realizar los reemplazos
-            char[] modifiedData = binaryData.ToCharArray();
-
-            for (int i = 0; i < modifiedData.Length; i++) {
-                if (modifiedData[i] == '0') {
-                    modifiedData[i] = '1';
-                }
-                else if (modifiedData[i] == '1') {
-                    modifiedData[i] = '0';
-                }
-            }
-            
-            // Convertir la cadena mutable de nuevo a una cadena
-            string result = new string(modifiedData);
-            return result;
-        }
-
-
-        #endregion
-        
-        #region SolveMistake
-
-        string SolveErrors(string sentData, string receivedData) {
-            if (sentData != receivedData) {
-                return "Error de transmisión";
-            }
-            
-            return "XD";
-        }
-        #endregion
-
-        #endregion
-
-        
     }
 }
