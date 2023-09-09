d70d0f56311e2290a8733204e00334a278289c60
angel <angelvasquez2570@gmail.com>
2023-09-08 21:16:19 -0600

    Interfaz gráfica redimensionable

--- BinaryDataManagement/Forms/ParidadForm.Designer.cs
+++ BinaryDataManagement/Forms/ParidadForm.Designer.cs
@@ -28,18 +28,114 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblDato = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles) ((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.richTextBox1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.lblDato);
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(14, 16);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(731, 608);
+            this.panel1.TabIndex = 2;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles) (((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Location = new System.Drawing.Point(61, 246);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(629, 190);
+            this.richTextBox1.TabIndex = 10;
+            this.richTextBox1.Text = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles) (((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.textBox1.Location = new System.Drawing.Point(274, 59);
+            this.textBox1.MaxLength = 7;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(227, 28);
+            this.textBox1.TabIndex = 9;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.label1.Location = new System.Drawing.Point(40, 117);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(223, 24);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Ingrese el tipo de paridad";
+            // 
+            // lblDato
+            // 
+            this.lblDato.AutoSize = true;
+            this.lblDato.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.lblDato.Location = new System.Drawing.Point(40, 57);
+            this.lblDato.Name = "lblDato";
+            this.lblDato.Size = new System.Drawing.Size(207, 24);
+            this.lblDato.TabIndex = 8;
+            this.lblDato.Text = "Ingrese el dato recibido";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles) (((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {"Seleccione una opción", "Paridad Par", "Paridad Impar"});
+            this.comboBox1.Location = new System.Drawing.Point(274, 115);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(227, 30);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles) (((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.button1.Location = new System.Drawing.Point(274, 160);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(227, 35);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Validar";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // ParidadForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(758, 641);
+            this.Controls.Add(this.panel1);
             this.Name = "ParidadForm";
             this.Text = "Paridad";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
-
         }
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblDato;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
 
         #endregion
     }
--- BinaryDataManagement/Forms/ParidadForm.cs
+++ BinaryDataManagement/Forms/ParidadForm.cs
@@ -15,6 +15,21 @@ namespace BinaryDataManagement.Forms
         public ParidadForm()
         {
             InitializeComponent();
+            richTextBox1.SelectionAlignment = HorizontalAlignment.Center;
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Obtén el carácter ingresado
+            char keyChar = e.KeyChar;
+
+            // Verifica si el carácter es 0, 1, Backspace o Delete
+            if (keyChar != '0' && keyChar != '1' && keyChar != '\b' && keyChar != (char)Keys.Delete)
+            {
+                // Si no es uno de los caracteres permitidos, suprime el evento de tecla
+                e.Handled = true;
+            }
         }
     }
 }
